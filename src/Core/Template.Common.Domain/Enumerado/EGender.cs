087acb8f87c0b25a324c83a24d7024cb69ab7c47
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-02-10 21:58:27 -0300

    Template.Common.Domain

---
+++ src/Core/Template.Common.Domain/Enumerado/EGender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template.Common.Domain.Enumerado;
+
+/// <summary>
+/// Represents the gender options.
+/// </summary>
+public enum EGender
+{
+    /// <summary>
+    /// Do not specify.
+    /// </summary>
+    [Description("Não informar")]
+    None = 0,
+
+    /// <summary>
+    /// Male gender.
+    /// </summary>
+    [Description("Male")]
+    Male = 1,
+
+    /// <summary>
+    /// Female gender.
+    /// </summary>
+    [Description("Female")]
+    Female = 2
+}
