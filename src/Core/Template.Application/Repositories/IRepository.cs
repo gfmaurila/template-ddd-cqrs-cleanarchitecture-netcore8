dad08ba0adfee57b1473a4d8ac615df6f346b1c3
Guilherme Figueiras Maurila <gfmaurila@gmail.com>
2025-02-13 20:53:18 -0300

    UserRepository

--- src/Core/Template.Application/Repositories/IRepository.cs
+++ src/Core/Template.Application/Repositories/IRepository.cs
@@ -4,7 +4,7 @@ public interface IRepository<TEntity> where TEntity : class
 {
     Task<TEntity?> GetByIdAsync(Guid id);
     Task<bool> IsExistsAsync(Guid id);
-    Task<IEnumerable<TEntity>> GetAllAsync();
+    Task<List<TEntity>> GetAllAsync();
     Task AddAsync(TEntity entity, CancellationToken cancellationToken);
     Task UpdateAsync(TEntity entity);
     Task DeleteAsync(Guid id);
--- src/Core/Template.Infrastructure/Persistence/Repositories/UserRepository.cs
+++ src/Core/Template.Infrastructure/Persistence/Repositories/UserRepository.cs
@@ -1,61 +1,50 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Template.Application.Repositories;
 using Template.Domain.Users;
+using Template.Infrastructure.Persistence.Entities;
 
 namespace Template.Infrastructure.Persistence.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private readonly TemplateAppDbContext _dbContext;
+    private readonly TemplateAppDbContext _context;
     private readonly IMapper _mapper;
-    public UserRepository(TemplateAppDbContext dbContext, IMapper mapper)
+    public UserRepository(TemplateAppDbContext context, IMapper mapper)
     {
-        _dbContext = dbContext;
+        _context = context;
         _mapper = mapper;
     }
 
     #region Commands
-    public Task AddAsync(User entity, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task AddAsync(User entity, CancellationToken cancellationToken)
+        => await _context.AddAsync(_mapper.Map<UserEntity>(entity));
 
     public Task UpdateAsync(User entity)
-    {
-        throw new NotImplementedException();
-    }
+        => Task.FromResult(_context.Update(entity));
 
     public Task DeleteAsync(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+        => Task.FromResult(_context.Remove(id));
     #endregion
 
     #region Queries
-    public Task<bool> ExistsByEmailAsync(string email)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<bool> ExistsByEmailAsync(string email)
+        => await _context.User.AsNoTracking().AnyAsync(entity => entity.Email == email);
 
-    public Task<IEnumerable<User>> GetAllAsync()
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<List<User>> GetAllAsync()
+        => _mapper.Map<List<User>>(await _context.User.AsNoTracking().ToListAsync());
 
-    public Task<User?> GetByIdAsync(Guid id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<bool> IsExistsAsync(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<User?> GetByIdAsync(Guid id)
+        => _mapper.Map<User>(await _context.User
+                         .Where(b => b.Id == id)
+                         .FirstOrDefaultAsync());
+    public async Task<bool> IsExistsAsync(Guid id)
+        => (await _context.User.FirstOrDefaultAsync(x => x.Id == id)) != null;
     #endregion
 
 
 
 
 
-    
+
 }
